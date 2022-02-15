c88e18255b22f153640b20b5c5ad0bd071784d1d
tudorstanciulescu <tudorstanciulescu@gmail.com>
2022-02-15 15:26:30 +0200

    add changes to 1vs1 lost

--- csgoHLTV/main.js
+++ csgoHLTV/main.js
@@ -1,14 +1,9 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let http = new XMLHttpRequest();
-let fs = require('fs');
-let cheerio = require('cheerio');
-let got = require('got');
-let makeSlug = require('./makeSlug');
-let saveRedis = require('./redis.js');
 let variables = require('./variables.json');
 let getTeamPlayers = require('./getTeamPlayers');
 let  kdOnMap  = require("./kdOnMap");
-  
+let get1vs1Lost = require('./get1vs1Lost');  
 var team = "Natus Vincere";
 
 async function getPlayerById() {
@@ -35,50 +30,30 @@ async function getPlayerById() {
 
   });
 //   console.log(playerObj);
- 
-  playersName.forEach(async (playerName) => {
- 
-        playerObject = {};
-        http.open("get", `https://www.hltv.org/api/event/player/${playerName}?startDate=2021-01-12&endDate=2021-07-01`, false, variables.username, variables.password);
-        http.send("");
-        
-        if (http.status == 200) {
+  
+  for (let i=0;i<playersName.length;i++) {
+        let playerName = playersName[i];
+      playerObject = {};
+      http.open("get", `https://www.hltv.org/api/event/player/${playerName}?startDate=2021-01-12&endDate=2021-07-01`, false, variables.username, variables.password);
+      http.send("");
+      
+      if (http.status == 200) {
 
-            let playerData = JSON.parse(http.responseText);
-            playerObject.on1lose = await get1vs1Lost(playerObj[playerName],playerName);
-            playerData.maps =  await kdOnMap.kdOnMap(playerName);
-            playerData.on1lost  = playerObject.on1lose;
-            // console.log(playerData);
-            var redisObj = JSON.stringify(playerData);
-            saveRedis.saveRedis(team, playerData.nick, redisObj);
+          let playerData = JSON.parse(http.responseText);
+          playerObject.on1lose = await get1vs1Lost.get1vs1Lost(playerObj[playerName],playerName);
+          playerData.maps =  await kdOnMap.kdOnMap(playerName);
+          playerData.on1lost  = playerObject.on1lose;
+          console.log(playerData);
+          var redisObj = JSON.stringify(playerData);
+          // saveRedis.saveRedis(team, playerData.nick, redisObj);
 
-        } else {
-            // console.log("⚠️ Authentication failed.");
-        }
+      } else {
+          // console.log("⚠️ Authentication failed.");
+      }
 
-  }) 
-
+  }
 }
 
 getPlayerById();
 
 
-
-async function get1vs1Lost(id,name) {
-
-    return new Promise((resolve, reject) => {
-
-var vgmUrl = `https://www.hltv.org/stats/players/clutches/${id}/1on1/${makeSlug.makeSlug(name)}?startDate=2021-01-12&endDate=2021-07-01`;
-
-got(vgmUrl).then(response => {
-var $ = cheerio.load(response.body);
-var div = Number(($(`.summary .col:nth-child(2) .value`).html()));
-playerObject.on1lose = div;
-    resolve(div);
-
-}).catch(err => {
-console.log(err);
-});
-
-})
-}
