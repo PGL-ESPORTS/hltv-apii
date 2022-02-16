2b98fb3a1fcf9c1d717651e4d7bb5b6ac97f94c1
tudorstanciulescu <tudorstanciulescu@gmail.com>
2022-02-16 17:17:41 +0200

    removed node modules

--- teamStats.js
+++ teamStats.js
@@ -1,21 +1,6 @@
 const { HLTV } = require('hltv')
 
-// HLTV.getMatchMapStats({ id: 123224 }).then((res) => {
-//     console.log(res.playerStats);
-//   })
-//   HLTV.getTeamStats({ id: 4608,
-//     startDate: "2021-01-01",
-//     endDate: "2021-07-07",
-//     rankingFilter: "Top10" }).then(res => {
-//     // console.log(Object.keys(res));
-//     console.log(res.matches);
-    
-// HLTV.getMatchStats({ id: 2349632 }).then((res) => {
-   
-//   })
 
-
-var mynumber = 1.232323232;
-
-var newNUmber = parseFloat(mynumber).toFixed(2);
- console.log(newNUmber);
+HLTV.getTeamStats({ id: 4608 }).then(res => {
+    console.log(res.overview);
+})
